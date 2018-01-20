744eb09a2da1bf73fa38e60a3035187c4e2fcd52
Frenchcooc <corentin.brossault@gmail.com>
2018-01-20 14:38:45 +0100

    Ignoring node_modules

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app     = express();
 const path    = require('path');
 const fs      = require('fs');
 
+app.set('PRODUCTION', ((process.env.PORT) ? true : false));
 app.set('port', (process.env.PORT || 3030));
 
 global.gandi = {};
@@ -15,7 +16,10 @@ app.use((req, res, next) =>
 
   if ('host' in req.headers)
   {
-    var host = req.headers['host'].split(':')[0];
+    var host = (app.get('PRODUCTION') ? req.headers['host'] : 'stationf.ovh');
+        host = host.split(':')[0];
+        host = (app.get('PRODUCTION') ? host : '../' + host);
+
     var vhost = path.join(__dirname, host);
 
     fs.access(vhost, fs.constants.R_OK, (err) =>
@@ -23,6 +27,10 @@ app.use((req, res, next) =>
       if (err) { showError (req, res); }
       else
       {
+        global.gandi.request = req;
+        global.gandi.response = res;
+        console.log('Vhost: ',vhost);
+        console.log('URL: ', req.originalUrl);
         var appVhost = require(vhost);
             appVhost(req, res, next);
       }
@@ -31,12 +39,10 @@ app.use((req, res, next) =>
   else { showError (req, res); }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), function() { console.log('Node app is running on port', app.get('port')); });
 
 function showError (req, res)
 {
   res.status(404);
-  res.send('<!DOCYTPE html><html><body>Vhost is not specified or unknown :\'(');
+  res.send('<!DOCYTPE html><html><body>Vhost is not specified or unknown - you may contact <a href="http://www.twitter.com/frenchcooc">@frenchcooc</a>\n');
 }
